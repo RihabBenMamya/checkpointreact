c333a9174b8f2b34ecf92851126068e1f2cc114b
rihab <rihab@1D-RIHABB.DC-CAPES.COM>
2021-05-26 12:00:14 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import './App.css';
 import Login from './components/Login';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-       
+       <Router>
        <Navbar />
        <header className="App-header bg-image"><h1>First React Checkpoint</h1>
        <p>Example page header</p>
        </header>
-       <Login/>
+
+       <Route path="/login"><Login/></Route>
+       
       <br />
      
       
       <Footer />
-     
+      </Router>
     </div>
   );
 }
